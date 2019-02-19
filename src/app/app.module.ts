aafb6014ca9db0848cebd7ff767d6e0e9139f351
NaokiNakamura <naoki.nakamura@comsize.com>
2019-02-20 00:18:39 +0900

    9_1まで完了

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { SimpleFormComponent } from './simple-form/simple-form.component';
 import { BetterFormComponent } from './better-form/better-form.component';
 import { ControlsComponent } from './controls/controls.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { ControlsComponent } from './controls/controls.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
